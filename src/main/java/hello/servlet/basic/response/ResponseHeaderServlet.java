08b87d90de4742f22e100358cd8c047049d5eab3
hojun <hjlee9303@gmail.com>
2023-08-05 20:30:16 +0900

    Feat: 웹 서블릿을 만들고, Http request와 response의 기본 동작 방식 요약      - 서블릿이 있기에 개발자는 중복되는 http 파싱 작업을 하지 않고 객체로 http 요청, 응답을 할 수 있다.      - 서블릿이 되기 위해 HttpServlet을 상속받아 service()를 오버라이드해야 한다.      - http 요청은 크게 header, body, cookie 3가지로 구성되어 객체를 통해 설정, 조회가 가능하다.

---
+++ src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -0,0 +1,23 @@
+package hello.servlet.basic.response;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "responseHeaderServlet", urlPatterns = "/response-header")
+public class ResponseHeaderServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        resp.setStatus(HttpServletResponse.SC_OK);
+
+        resp.setContentType("text/plain;charset=utf-8");
+        resp.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        resp.setHeader("Pragma", "no-cache");
+        resp.setHeader("my-header", "hello");
+
+        resp.getWriter().write("ok");
+    }
+}
