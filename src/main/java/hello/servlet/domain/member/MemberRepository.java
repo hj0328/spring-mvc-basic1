ecc6a2a4ae7186757748a740c08b85dea0c603dd
hojun <hjlee9303@gmail.com>
2023-08-06 22:42:11 +0900

    Feat: 회원 가입, 조회 기능 추가  - 서블릿만 사용하여 http 요청(member의 가입, 조회)처리  - http 요청응답 객체로 요청 조회와 응답 만들기가 편하지만 여전히 중복되는 코드가 존재  - html코드가 서블릿 내에 존재하여 유지보수 안좋다

---
+++ src/main/java/hello/servlet/domain/member/MemberRepository.java
@@ -0,0 +1,39 @@
+package hello.servlet.domain.member;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class MemberRepository {
+
+    private static ConcurrentHashMap<Long, Member> store = new ConcurrentHashMap<>();
+    private static AtomicLong sequence = new AtomicLong();
+
+    private static final MemberRepository instance = new MemberRepository();
+
+    public static MemberRepository getInstance() {
+        return instance;
+    }
+
+    private MemberRepository() {
+    }
+
+    public Member save(Member member) {
+        member.setId(sequence.incrementAndGet());
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Member findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
