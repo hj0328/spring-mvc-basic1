4b55c16ce89a05c1238e5cc8908a577dbf4ac3c5
hojun <hjlee9303@gmail.com>
2023-08-25 19:45:37 +0900

    Feat: 로그인 서비스 도메인 추가 (진행중)  - 메모리 repository 추가  - 회원 도메인 추가  - 로그인 컨트롤러 양식 추가  - 로그인 타임리프 추가(수업자료)

---
+++ item-service/src/main/java/hello/itemservice/domain/login/LoginService.java
@@ -0,0 +1,22 @@
+package hello.itemservice.domain.login;
+
+import hello.itemservice.domain.member.Member;
+import hello.itemservice.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+    private final MemberRepository memberRepository;
+
+    /**
+     * 로그인 실패하면 null 리턴
+     */
+    public Member login(String loginId, String password) {
+        return memberRepository.findByLoginId(loginId)
+                .filter(m -> m.getPassword().equals(password))
+                .orElse(null);
+    }
+}
