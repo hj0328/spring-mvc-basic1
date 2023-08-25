942abf451a6e2a69ea3012e003c7e673172a9ed6
hojun <hjlee9303@gmail.com>
2023-08-25 19:15:31 +0900

    Feat: 회원가입 도메인, 컨트롤러, 홈 추가(진행중)

--- item-service/src/main/java/hello/itemservice/domain/item/ItemController.java
+++ item-service/src/main/java/hello/itemservice/domain/item/ItemController.java
@@ -101,13 +101,4 @@ public class ItemController {
         itemRepository.update(itemId, item);
         return "redirect:/view/items/{itemId}";
     }
-
-    /*2
-        테스트용 데이터 추가
-     */
-    @PostConstruct
-    public void init() {
-        itemRepository.save(new Item("itemA", 10000, 10));
-        itemRepository.save(new Item("itemB", 20000, 20));
-    }
 }
--- item-service/src/main/java/hello/itemservice/web/HomeController.java
+++ item-service/src/main/java/hello/itemservice/web/HomeController.java
@@ -11,6 +11,6 @@ public class HomeController {
     @RequestMapping("/")
     public String home() {
         log.info("Redirect /items");
-        return "redirect:/view/items";
+        return "home";
     }
 }
